737c6aa7f96194dca253d7b4a0a7559ade005209
Priansh Shah <shah@priansh.com>
2018-01-28 03:09:11 +0000

    app fin

--- webapp/Deckboi/App.js
+++ webapp/Deckboi/App.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import Camera from 'react-native-camera';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { looks: "Calibrating..." }    
+    this.state = { 
+      looks: "Calibrating...",
+      token: "", text: "", frameId: 1, set: false
+    } // index by 1 because !frameId == !1 == 0 = true for first while 0 --> false
   }
   componentDidMount() {
     // hacking thru cDM because i can't be bothered to polyfill :0 soz
-    this.timer = setInterval(() => this.notTakePicture(), 10 * 1000);
+    if(this.state.set)
+      this.timer = setInterval(() => this.notTakePicture(), 10 * 1000);
   }
   render() {
     return (
       <View style={styles.container}> 
-        <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          style={styles.preview}
-          captureTarget={Camera.constants.CaptureTarget.memory}
-          aspect={Camera.constants.Aspect.fill}>
-          <Text style={styles.capture} onPress={() => this.notTakePicture()}>fam :3 we got dis</Text>
-          <Text style={styles.capture}>{this.state.looks}</Text>
-        </Camera>
+        {
+          (this.state.set && 
+          <Camera
+            ref={(cam) => {
+              this.camera = cam;
+            }}
+            style={styles.preview}
+            captureTarget={Camera.constants.CaptureTarget.memory}
+            aspect={Camera.constants.Aspect.fill}>
+            <Text style={styles.capture} onPress={() => this.notTakePicture()}>fam :3 we got dis</Text>
+            <Text style={styles.capture}>{this.state.looks}</Text>
+          </Camera>) ||
+          <View style={{flex:1, alignItems: 'center', justifyContent:'center'}}>
+            <TextInput style={{width: 350}} placeholder={"event name"} value={this.state.text} onChangeText={(t) => { this.setState({text: t}) }} />
+            <Button title={"Begin"} style={{width: 200, marginBottom: 200}} onPress={() => {
+              this.timer = setInterval(() => this.notTakePicture(), 10 * 1000);
+              this.setState({
+                looks: this.state.looks,
+                token: this.state.text, text: this.state.text, set: true
+              })
+            }}><Text>Begin</Text></Button>
+          </View>
+        }
       </View>
     );
   }
@@ -48,11 +65,25 @@ export default class App extends React.Component {
               this.state.looks = num + " ppl, " + d.top2[0] + " & " + d.top2[1]
             else
               this.state.looks = num + " ppl, " + d.top2[0]
+            delete d.top2
+            let z = { size: num, emotions: d}
+            fetch('http://192.81.214.158:6666/realtime', {
+              method: 'POST',
+              body: JSON.stringify({ 
+                token: this.state.token, 
+                frameId: this.state.frameId, 
+                scores: z,
+              }),
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
+            }).then((s) => console.log(s)).catch((a)=>console.log(a))
           }
           else {
             this.state.looks = "Don't see anyone :/"
           }
-          this.setState({looks: this.state.looks})
+          this.setState({looks: this.state.looks, frameId: this.state.frameId + 1})
         })
       })
       .catch(err => console.error(err));
